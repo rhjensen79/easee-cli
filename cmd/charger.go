62c1bbbfb811e0daaaa689291757388c30a39329
Robert Jensen <robert@robert-jensen.dk>
2022-12-30 08:52:34 +0100

    Added default command to charger

--- cmd/charger.go
+++ cmd/charger.go
@@ -4,6 +4,8 @@ Copyright © 2022 Robert Jensen https://github.com/rhjensen79
 package cmd
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,13 @@ var chargerCmd = &cobra.Command{
 	Use:   "charger",
 	Short: "The commands assosiated with the charger",
 	Long:  `The commands assosiated with the charger`,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 
 	},
